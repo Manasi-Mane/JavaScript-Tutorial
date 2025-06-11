77f97fd240277c9265cce2f30866991272f09f7b
Manasi Mane <manasimane711@gmail.com>
2025-06-11 05:04:05 +0000

    added variables and dataType files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 18665
+let accountEmail="manasimane@gmail.com"
+var accountPassword="12345"
+accountCity="Satara"
+let accountState
+// accountId=3  //not allowed
+console.log(accountId);
+accountEmail="mm@gmail.com"
+accountPassword="232123"
+accountCity="Pune"
+console.table([accountId, accountEmail, accountPassword, , accountState])
