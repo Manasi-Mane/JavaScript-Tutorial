a1b5aabd5a5c10199a7f2b42d56ac97d45f1f1a3
Manasi Mane <manasimane711@gmail.com>
2025-06-13 07:58:05 +0000

    comparizons

---
+++ 01_basics/04_comparizons.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false  equality and comparizon work differently
+// console.log(null >= 0);  // true   comparizon convert null to zero 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// strict check ===(check data type alse along with values)
+console.log("2"===2); //false
