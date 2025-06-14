52fe666109d8df6facf6a8d02310c465903fc2f9
Manasi Mane <manasimane711@gmail.com>
2025-06-14 06:13:31 +0000

    sumarry expanded

--- 01_basics/05_dataTypes_summary.js
+++ 01_basics/05_dataTypes_summary.js
@@ -1,5 +1,31 @@
 //Primitive data types
 // 7 categories : Strings, Number, Boolean, null(empty), undefined, Symbol, BigInt
 
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp= null
+let userEmail;
+
+const id =  Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+const bigNumber = 7276215587267356487898767677878n
+console.log(typeof bigNumber);
+
 //Referenced (Non Primitive)
 // Arrays, Objects, Functions
+
+const heros = ["Shaktiman" , "naagraj", "doga"]
+let myObj = {
+     name:"manasi",
+     age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");   
+}
+
+console.log(typeof myObj);  // function object
