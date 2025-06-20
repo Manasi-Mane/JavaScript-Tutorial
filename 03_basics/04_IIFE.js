3400cd471f40bbcde254cba58de567cc0f815b9c
Manasi Mane <manasimane711@gmail.com>
2025-06-20 10:12:35 +0000

    IIFE 2

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,1 +1,11 @@
-//Immediately invoked Function Expression (IIFE)
+//Immediately invoked Function Expression (IIFE) //avoids global scope pollution
+// ()()
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) =>  {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )("Manasi")
