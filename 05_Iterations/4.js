03229ffe4c7c14677e58f5ea4c6c5cf0f75bbd94
Manasi Mane <manasimane711@gmail.com>
2025-06-21 13:05:47 +0000

    filter, map, reduce

--- 05_Iterations/4.js
+++ 05_Iterations/4.js
@@ -11,6 +11,17 @@ for (const key in myObject) {
 
 const programming =["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
-    console.log(key + ":- "+programming[key]);
+    // console.log(key + ":- "+programming[key]);
     
 }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', " France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
